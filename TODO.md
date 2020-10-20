ff1731f5a9a167d21a563e04ce1bafc5936290ff
Jeffrey Ryan <jeffaryan7@gmail.com>
2020-10-20 11:17:23 -0500

    Updated TODO and make upload_test.sh usable

--- TODO.md
+++ TODO.md
@@ -2,4 +2,6 @@
 
 * Lookup stealth addresses for every primary address in wallet
 * Handle cache output errors more gracefully
-
+* GUI
+* Figure out how to read .bin RPC responses. This could reduce bandwidth by 5x
+* Validate blockchain as it is scanning? (HARD)
--- upload_test.sh
+++ upload_test.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
+FILE1="dist/$(ls -t dist | head -n2 | head -n1)"
+FILE2="dist/$(ls -t dist | head -n2 | tail -n1)"
 python setup.py sdist bdist_wheel
-python -m twine upload --repository testpypi dist/*
+python -m twine upload --repository testpypi "$FILE1" "$FILE2"
